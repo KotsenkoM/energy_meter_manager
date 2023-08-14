286014743cc18cf4619e6f418c7bed17c94ae10a
KotsenkoM <maksimkcenk93@gmail.com>
2023-08-14 19:07:10 +0300

    Добавлен метод для вывода статистики по счетчику за указанный временной промежуток

--- counters/api/views.py
+++ counters/api/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Sum
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import EnergyMeter, EnergyMeasurements
-from .serializers import EnergyMeterSerializer
+from .serializers import EnergyMeterSerializer, EnergyMeasurementsSerializer
 
 
 class EnergyMeterViewSet(viewsets.ViewSet):
@@ -35,6 +36,29 @@ class EnergyMeterViewSet(viewsets.ViewSet):
                 'number': energy_meter.number,
                 'amps': current_measurement.amps if current_measurement else None,
                 'kilowatt': total_kilowatt if total_kilowatt else 0,
+            }
+
+            return Response(response_data)
+        except EnergyMeter.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=True, methods=['GET'])
+    def statistics(self, request, pk=None):
+        try:
+            energy_meter = EnergyMeter.objects.get(pk=pk)
+            start_date = request.query_params.get('start_date')
+            end_date = request.query_params.get('end_date')
+
+            measurements = EnergyMeasurements.objects.filter(
+                number=energy_meter.number,
+                date__range=(start_date, end_date)
+            )
+            total_kilowatt = measurements.aggregate(total_kilowatt=Sum('kilowatt'))['total_kilowatt']
+
+            response_data = {
+                'number': energy_meter.number,
+                'total_kilowatt': total_kilowatt if total_kilowatt else 0,
+                'measurements': EnergyMeasurementsSerializer(measurements, many=True).data
             }
 
             return Response(response_data)
